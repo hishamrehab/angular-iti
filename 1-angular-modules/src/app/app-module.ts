def25acda6ee1906fc7e956e3b49759463fe4121
hishamrehab <hishamrehab30@gmail.com>
2026-01-27 05:19:37 +0200

    Navbar Component

--- 1-angular-modules/src/app/app-module.ts
+++ 1-angular-modules/src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { Footer } from './components/footer/footer';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar,
+    Home,
+    Footer
   ],
   imports: [
     BrowserModule,
--- 1-angular-modules/src/app/app.ts
+++ 1-angular-modules/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-root', // directive
+  selector: 'app-root',// directive
   templateUrl: './app.html',
   standalone: false,
   styleUrl: './app.css'
