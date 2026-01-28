f4ea23967c5b7aa25c17786193935ce74ab8bae0
hishamrehab <hishamrehab30@gmail.com>
2026-01-28 19:26:02 +0200

    Products Component

---
+++ 2-ecommerce-app/ecommerceApp/src/app/components/products/products.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { Iproduct } from '../../models/iproduct';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule],
+  templateUrl: './products.html',
+  styleUrl: './products.css',
+})
+export class Products {
+ products:Iproduct[]
+ constructor() {
+  this.products = [
+ {
+  id: 100 
+  , name : "Dell laptop" 
+  , price: 500000 
+  , quantity: 3 
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 1
+  },
+   {
+  id: 200 
+  , name : "HP laptop" 
+  , price: 100000 
+  , quantity: 10
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 1
+  },
+
+  {
+  id: 300 
+  , name : "Iphone" 
+  , price: 10000 
+  , quantity: 100
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 2
+  },
+
+  
+  {
+  id: 400 
+  , name : "oppo" 
+  , price: 60000 
+  , quantity: 1
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 2
+  },
+
+
+  {
+  id: 500 
+  , name : "samosong tablet" 
+  , price: 20000 
+  , quantity: 0
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 3
+  },
+  {
+  id: 600 
+  , name : "lenovo tablte" 
+  , price: 60000 
+  , quantity: 4
+  , imgUrl: "https://picsum.photos/id/1/200/300",
+    catId: 3
+  }
+  ]
+ }
+}
